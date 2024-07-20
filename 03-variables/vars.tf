be611ea186f2b1280dd9663f65994b451a4328f9
Akhil <akhilmahendra035@gmail.com>
2024-07-20 23:04:25 +0530

    vars demo

--- 03-variables/vars.tf
+++ 03-variables/vars.tf
@@ -6,11 +6,27 @@ variable "x" {
 # Declare variable without value
 #variable "y" {}
 
+# declaring variable with list type
 variable "y_list" {
   default = [1,2,3]
 }
 
+
+# declaring variable with map type
+variable "z_map" {
+  default = {
+    x = 10
+    y = 20
+    z = "akhil"
+  }
+}
+
+
 # print the variable
 output "y_list" {
   value = var.y_list
+}
+
+output "z_map" {
+  value = var.z_map
 }
