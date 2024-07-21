1adff38534389b4a479add192767a4d8ed059fc1
Akhil <akhilmahendra035@gmail.com>
2024-07-21 15:15:56 +0530

    conditions

---
+++ 06-condition/main.tf
@@ -0,0 +1,8 @@
+# condition in terraform is all about choosing the value
+# condition ? true value : false value
+
+resource "aws_instance" "test" {
+  ami           = "ami-041e2ea9402c46c32"
+  instance_type = var.instance_type == "t2.*" ? "t3.micro" : var.instance_type
+}
+variable "instance_type" {}
