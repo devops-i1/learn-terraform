8728acde9155592471ce952461438b6dd0f190e3
Akhil <akhilmahendra035@gmail.com>
2024-07-21 15:22:12 +0530

    conditions

--- 06-condition/main.tf
+++ 06-condition/main.tf
@@ -1,8 +1,13 @@
 # condition in terraform is all about choosing the value
 # condition ? true value : false value
 
-resource "aws_instance" "test" {
-  ami           = "ami-041e2ea9402c46c32"
-  instance_type = var.instance_type == "t2.*" ? "t3.micro" : var.instance_type
+#resource "aws_instance" "test" {
+#  ami           = "ami-041e2ea9402c46c32"
+#  instance_type = var.instance_type == "t2.*" ? "t3.micro" : var.instance_type
+#}
+#variable "instance_type" {}
+
+output "number" {
+  value = var.number > 10 ? "number is > 10" : "number is <= 10"
 }
-variable "instance_type" {}
+variable "number" {}
