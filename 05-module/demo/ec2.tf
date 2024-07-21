ecb69283755eb92d917c3c4642209e255c57e960
Akhil <akhilmahendra035@gmail.com>
2024-07-21 14:38:45 +0530

    module demo

--- 05-module/demo/ec2.tf
+++ 05-module/demo/ec2.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "test" {
   ami           = "ami-041e2ea9402c46c32"
-  instance_type = "t3.micro"
+  instance_type = var.instance_type
   tags = {
     Name = var.Name
   }
 }
 
 variable "Name" {}
+variable "instance_type" {}
